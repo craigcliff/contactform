4d5ea74a17d0d9a810755c64ffad589a439e70bd
Craig <Craigcliff@gmail.com>
2018-07-05 21:07:41 +0200

    first commit

---
+++ handler.js
@@ -0,0 +1,17 @@
+'use strict';
+// process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
+module.exports.staticSiteMailer = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+};
